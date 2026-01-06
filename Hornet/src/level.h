393b4777eb4a8f89e4b286221f8c47063c07727f
Mason24727 <w24003356@northumbria.ac.uk>
2026-01-06 13:29:28 +0000

    added player with literal valuse to test functionality

--- Hornet/src/level.h
+++ Hornet/src/level.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "../GameObjects/player.h"
 class Level
 {
 public:
@@ -9,6 +10,9 @@ public:
 
 	bool LoadLevel()
 	{
+		Player* player = new Player(ObjectType::PLAYER);
+		player->Initialise("../Program/assets/Rocket1.png",Vector2D(1000, 1000), 0, 0.12);
+		ObjectManager::instance.AddItem(player);
 		return true;
 	}
 };
